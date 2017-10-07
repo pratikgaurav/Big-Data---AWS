5f5c299a87c3defda0555668a4e67f7154f23d60
GAURAV VENKATA SATYA PRATIK KAMIREDDI <pratikgaurav@users.noreply.github.com>
2017-10-07 06:12:54 -0500

    Create whitehouse1.py
    The above python code gives the top 10 most frequent visitors (NAMELAST, NAMEFIRST, NAMEMID) to the White
    House in the year 2012.
    The dataset is taken from the below website under the year 2012 which is a .csv zip file of size 28.4mb:
    https://obamawhitehouse.archives.gov/goodgovernment/tools/visitor-records

---
+++ whitehouse1.py
@@ -0,0 +1,47 @@
+import os
+os.environ['S3_USE_SIGV4'] = 'True'
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import csv
+import operator
+import io
+
+output=[]
+seen = set()
+l = 0
+class MRMostUsedWord(MRJob):
+    def mapper_get_words(self,_,tweet):
+        with open('/home/ubuntu/mrjobs/WhiteHouse-WAVES-2012.csv', encoding="latin-1") as cvsfile:
+            reader = csv.reader(cvsfile)
+            if l == 0:
+                for i,row in enumerate(reader):
+                    global l
+                    l = 1
+                    yield '%s %s %s' % (row[0],row[1],row[2]), 1
+                    
+
+
+    def combiner_count_words(self, word, counts):
+        yield (word, sum(counts))
+
+    def reducer_count_words(self, word, counts):
+        output.append((sum(counts),word))
+        output.sort(key=lambda r:r[0],reverse = True)
+
+    def reducer_find_Top_Visitors(self):
+        for i,result in enumerate(output):
+            if i<=9:
+                yield result
+           
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_words,combiner=self.combiner_count_words,
+                   reducer=self.reducer_count_words),MRStep(reducer_final=self.reducer_find_Top_Visitors)
+        ]
+
+
+if __name__ == '__main__':
+    MRMostUsedWord.run()
+
+del os.environ['S3_USE_SIGV4']
